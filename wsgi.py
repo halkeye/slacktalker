d9e5fe53d385ded743827386392971faa97d177f
Gavin Mogan <git@gavinmogan.com>
2019-07-02 16:53:34 -0700

    Enable flask integration everywhere

--- wsgi.py
+++ wsgi.py
@@ -4,12 +4,16 @@ import sentry_sdk
 
 from slack_resurrect.model import create_all
 from slack_resurrect.settings import CONFIG
-from slack_resurrect.web import app
+from slack_resurrect.web import app, FlaskIntegration
 
 LOG = logging.getLogger(__name__)
 
 if CONFIG.SENTRY_TOKEN:
-    sentry_sdk.init(CONFIG.SENTRY_TOKEN, environment=CONFIG.SENTRY_ENVIRONMENT)
+    sentry_sdk.init(
+        dsn=CONFIG.SENTRY_TOKEN,
+        environment=CONFIG.SENTRY_ENVIRONMENT,
+        integrations=[FlaskIntegration()]
+    )
 
 try:
     create_all()
