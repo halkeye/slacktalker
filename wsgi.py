bc3f515045e04a2cd7cf90de646c798a4b2347e8
Gavin Mogan <git@gavinmogan.com>
2019-07-02 22:41:56 -0700

    Add debugging logging, and make logging more smart

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,9 @@
 import logging
-
 import sentry_sdk
 
 from slack_resurrect.settings import CONFIG
 from slack_resurrect.web import create_app, sentry_integrations
 from slack_resurrect.db import db
-
-LOG = logging.getLogger(__name__)
 
 if CONFIG.SENTRY_TOKEN:
     sentry_sdk.init(
@@ -16,18 +13,28 @@ if CONFIG.SENTRY_TOKEN:
     )
 
 
-app = create_app()
+app = None
 try:
+    app = create_app()
+
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
+    app.logger.info(CONFIG.toJSON())
     with app.app_context():
         db.create_all()
+    if __name__ == '__main__':
+        app.run(
+            debug=CONFIG.DEBUG,
+            port=int(CONFIG.PORT),
+            host="0.0.0.0"
+        )
 except Exception as exp:
-    LOG.error("Exception occurred", exc_info=True)
+    if app:
+        app.logger.error("Exception occurred", exc_info=True)
+    else:
+        print(exp)
     sentry_sdk.capture_exception(exp)
     raise SystemExit()
 
-if __name__ == '__main__':
-    app.run(
-        debug=CONFIG.DEBUG,
-        port=int(CONFIG.PORT),
-        host="0.0.0.0"
-    )
